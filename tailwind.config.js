ffde3ee1a9630b6bccab7dc4da6fb67495b72cd3
Oğuz Adıgüzel <oguz_adiguzel@outlook.com>
2023-02-27 18:14:10 +0300

    added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+        
+      }
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors: {
+        'gega-red': '#BC1A45',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+
+    },
   },
   plugins: [],
 }
